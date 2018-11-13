ac82dc742db4fc649143bc1baab7ae2d60ed2f62
anshul <its-Anshul>
2018-11-13 09:25:53 +0530

    Compilation MicroService done

---
+++ Compilation/src/main/java/com/sc200/service/CompileService.java
@@ -0,0 +1,11 @@
+package com.sc200.service;
+
+import com.sc200.domain.File;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public interface CompileService {
+
+    public ArrayList<String> runFile(File  file) throws IOException;
+}

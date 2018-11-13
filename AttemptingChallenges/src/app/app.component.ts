7e1cb3ada635f17c0c17b759be42928f0888b31b
CGI-praneeth <praneethvikram635@gmail.com>
2018-11-13 11:05:40 +0530

    attempting challenges added

---
+++ AttemptingChallenges/src/app/app.component.ts
@@ -0,0 +1,127 @@
+import { Component } from '@angular/core';
+import { FileElement } from './folder-structure/directory/model/file-element';
+import { FileService } from './folder-structure/directory/file.service';
+import { Observable } from 'rxjs';
+import { FilesService } from './files.service'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AttemptingChallenges';
+
+  public fileElements: Observable<FileElement[]>;
+
+  files: File[];
+  
+  currentRoot: FileElement;
+  currentPath: string;
+  canNavigateUp = false;
+
+  constructor(public fileService: FileService, public filesService: FilesService) { }
+  
+ 
+  ngOnInit() {
+    this.fileElements = this.filesService.allFiles;
+    console.log(this.fileElements);
+    
+     //const folderA = this.fileService.add({ name: 'Folder A', isFolder: true, parent: 'root' ,url:null,content:null});
+     //this.fileService.add({ name: 'Folder B', isFolder: true, parent:folderA.id,url:null,content:null});
+     this.updateFileElementQuery();
+     //console.log(folderA.id);
+    }
+    private fileElement_array : FileElement[];
+    
+
+  addFile(file: { name: string, parent:string,url:string,id:string }) {
+    this.fileService.addFiles({id:file.id, isFolder: false, name: file.name, parent:file.parent,url:file.url,content:null });
+    this.updateFileElementQuery();
+  }
+
+
+
+  addFolder(folder: { name: string,parent:string,url:string,id:string }) {
+    this.fileService.add({id:folder.id, isFolder: true, name: folder.name, parent: folder.parent,url:folder.url,content:null });
+    this.updateFileElementQuery();
+  }
+  onShow(){
+    this.fileElement_array = this.fileService.fileElements_array;
+
+    //need to change the parent from string to id of its parent.
+
+   
+
+
+
+
+    for(let i=0;i<this.fileElement_array.length;i++){
+      if(this.fileElement_array[i].isFolder){
+        this.addFolder(this.fileElement_array[i]);
+        this.updateFileElementQuery();
+      }
+      else{
+        
+        this.addFile(this.fileElement_array[i]);
+        this.updateFileElementQuery();
+      }
+    }
+  
+  }
+
+ 
+
+
+  removeElement(element: FileElement) {
+    this.fileService.delete(element.id);
+    this.updateFileElementQuery();
+  }
+
+  navigateToFolder(element: FileElement) {
+    this.currentRoot = element;
+    this.updateFileElementQuery();
+    this.currentPath = this.pushToPath(this.currentPath, element.name);
+    this.canNavigateUp = true;
+  }
+
+  navigateUp() {
+    if (this.currentRoot && this.currentRoot.parent === 'root') {
+      this.currentRoot = null;
+      this.canNavigateUp = false;
+      this.updateFileElementQuery();
+    } else {
+      this.currentRoot = this.fileService.get(this.currentRoot.parent);
+      this.updateFileElementQuery();
+    }
+    this.currentPath = this.popFromPath(this.currentPath);
+  }
+
+  moveElement(event: { element: FileElement; moveTo: FileElement }) {
+    this.fileService.update(event.element.id, { parent: event.moveTo.id });
+    this.updateFileElementQuery();
+  }
+
+  renameElement(element: FileElement) {
+    this.fileService.update(element.id, { name: element.name });
+    this.updateFileElementQuery();
+  }
+
+  updateFileElementQuery() {
+    this.fileElements = this.fileService.queryInFolder(this.currentRoot ? this.currentRoot.id : 'root');
+  }
+
+  pushToPath(path: string, folderName: string) {
+    let p = path ? path : '';
+    p += `${folderName}/`;
+    return p;
+  }
+
+  popFromPath(path: string) {
+    let p = path ? path : '';
+    let split = p.split('/');
+    split.splice(split.length - 2, 1);
+    p = split.join('/');
+    return p;
+  }
+}
---
+++ AttemptingChallenges/src/app/files.service.ts
@@ -0,0 +1,101 @@
+import { Injectable } from '@angular/core';
+//import { FileService } from './folder-structure/directory/file.service';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { File } from './folder-structure/directory/model/file'
+import { containsElement } from '@angular/animations/browser/src/render/shared';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Authorization': 'my-auth-token'
+  })
+};
+@Injectable({
+  providedIn: 'root'
+})
+export class FilesService {
+
+  constructor(private http: HttpClient,) { }
+ 
+  allFiles;
+  textFiles;
+  fileContent;
+  files:[string];
+  url = "localhost:8020/file/create";
+  url1 = "172.23.239.117:8021/compile";
+  newurl:string = "";
+
+
+  StoreFiles(allFiles , textFiles , fileContent){
+    this.allFiles = allFiles;
+    this.textFiles = textFiles;
+    this.fileContent = fileContent;
+  }
+
+  DisplayFiles(){
+    console.log(this.textFiles);
+    console.log(this.fileContent);
+    console.log(this.allFiles);
+
+  
+      
+  }
+
+  SaveFile(file){
+    
+    // var file : File;
+    // file.url = url;
+    // file.content = content;
+    
+    //console.log(file.content);
+    console.log(file);
+    return this.http.post(this.url, file, httpOptions);
+
+  }
+
+  RunFile(file){
+    
+    // var file : File;
+    // file.url = url;
+    // file.content = content;
+    
+    //console.log(file.content);
+   // console.log(file.uri);
+      this.newurl= this.GetFilePath(file.uri);
+    console.log(this.newurl);
+    return this.http.post(this.url1,this.newurl, httpOptions);
+
+  }
+
+  GetAllFiles(){
+    console.log(this.allFiles);
+    return this.allFiles;
+  }
+ 
+
+  GetContent(fileName){
+
+    console.log(fileName);
+    var index;
+    for(var i=0; i < this.textFiles.length; i++ )
+    {
+      var temp = this.textFiles[i];
+      if(fileName == this.textFiles[i]){
+          index = i;
+    }
+
+    return this.fileContent[index];
+
+  }
+}
+GetFilePath(fileName){
+   this.files = this.GetAllFiles();
+  for(let i=0;i<this.files.length;i++){
+    if(this.files[i].includes(fileName)){
+      console.log(this.files[i]);
+      return this.files[i];
+    }
+  }
+}
+}

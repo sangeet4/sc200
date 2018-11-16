6572ee2c85b638db2b0f40f30bc76abf23afeb7f
anshul <its-Anshul>
2018-11-16 14:24:23 +0530

    All microservices linked through parent pom

--- Neo4jService/src/main/java/com/stackroute/neo4jservice/repository/relationrepository/SubPartRepository.java
+++
@@ -1,7 +0,0 @@
-package com.stackroute.neo4jservice.repository.relationrepository;
-
-import com.stackroute.neo4jservice.domain.relation.SubPartRelation;
-import org.springframework.data.neo4j.repository.Neo4jRepository;
-
-public interface SubPartRepository extends Neo4jRepository<SubPartRelation,String> {
-}
--- Neo4jService/src/main/java/com/stackroute/neo4jservice/repository/relationrepository/TypeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.stackroute.neo4jservice.repository.relationrepository;
-
-import com.stackroute.neo4jservice.domain.relation.TypeRelation;
-import org.springframework.data.neo4j.repository.Neo4jRepository;
-
-public interface TypeRepository extends Neo4jRepository<TypeRelation,String> {
-}

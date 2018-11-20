23ced1996fc2e45d060708238dc3b3ae93573fee
sangeet4 <and.sangeet@gmail.com>
2018-11-20 19:33:53 +0530

    dont know what i'm doing

---
+++ Scoring/src/main/java/com/stackroute/controller/ScoringController.java
@@ -0,0 +1,11 @@
+package com.stackroute.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/results")
+@CrossOrigin
+public class ScoringController {
+}
---
+++ Scoring/src/main/java/com/stackroute/domain/Profile.java
@@ -0,0 +1,16 @@
+package com.stackroute.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Profile {
+    private String userId;
+    private String challengeId;
+    private double questScore;
+}

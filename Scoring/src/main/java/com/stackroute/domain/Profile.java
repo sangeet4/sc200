0224fe17b134cb2c17d5e3460dd1091907aaeb8c
sangeet4 <and.sangeet@gmail.com>
2018-11-22 16:32:57 +0530

    scoring done as of now

--- Scoring/src/main/java/com/stackroute/domain/Profile.java
+++ Scoring/src/main/java/com/stackroute/domain/Profile.java
@@ -12,5 +12,6 @@ import lombok.NoArgsConstructor;
 public class Profile {
     private String userId;
     private String challengeId;
+    private String challengeTitle;
     private double questScore;
 }

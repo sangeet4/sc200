f9bc1e594b39bf03a9de803d97270e402bd91009
anshul.chauhan <anshul.chauhan@cgi.com>
2018-11-28 09:40:22 +0530

    Update dockerize.sh

--- FileParser/Directory/dockerize.sh
+++ FileParser/Directory/dockerize.sh
@@ -1,12 +1,7 @@
 #! /bin/bash
-cd ..
-docker-compose scale empty-container=1
-docker exec -it empty-container bash
-cd challenges/program
-mvn clean package
-cd ..
-rm -rf program
-cd ..
-exit
-docker-compose scale empty-container=0
+
+ssh ubuntu@ip-10-20-1-4 "docker-compose scale empty-container=1"
+#ssh ubuntu@<private-ip> "docker exec -it empty-container bash"
+ssh root@ip-10-20-1-4 "cd challenges/program ; mvn clean package ; cd .. ; rm -rf program ; cd .. "
+ssh ubuntu@ip-10-20-1-4 "docker-compose scale empty-container=0"
 

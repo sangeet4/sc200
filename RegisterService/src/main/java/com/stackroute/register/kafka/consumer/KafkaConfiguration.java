d653640760d81c7e4da987ce39876c1c6087e09f
faizan.rizwi <faizan.rizwi@cgi.com>
2018-11-29 15:56:42 +0530

    Update KafkaConfiguration.java

--- RegisterService/src/main/java/com/stackroute/register/kafka/consumer/KafkaConfiguration.java
+++ RegisterService/src/main/java/com/stackroute/register/kafka/consumer/KafkaConfiguration.java
@@ -25,7 +25,7 @@ public class KafkaConfiguration {
     public ConsumerFactory<String, String> consumerFactory() {
 
         Map<String, Object> config = new HashMap<>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "35.154.116.88:9092");
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
@@ -43,7 +43,7 @@ public class KafkaConfiguration {
     public ConsumerFactory<String, User> userConsumerFactory() {
         Map<String, Object> config = new HashMap<>();
 
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "35.154.116.88:9092");
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id6");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 //        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);

ed97ffedb38e98ea4a64b2c52e35e913fbc6ce83
anshul <its-Anshul>
2018-11-29 15:19:34 +0530

    tried error handling

--- FileParser/Directory/dockerize.sh
+++ FileParser/Directory/dockerize.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
-	echo $(cp challenges/Dockerfile challenges/$1/$2)
+	cp challenges/Dockerfile challenges/$1/$2 |& tee output.txt
         echo $(javac challenges/writeToFile.java)
 	echo $(java -cp "challenges:." writeToFile $1 $2 $3)
 	echo $(docker-compose up --build $1-$2)
+	echo $(output.txt)
 

597f10c144d093ba78226a0522ca34232d09b363
anshul <its-Anshul>
2018-11-29 16:27:43 +0530

    Error handling done in scripts

--- FileParser/Directory/dockerize.sh
+++ FileParser/Directory/dockerize.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 	cp challenges/Dockerfile challenges/$1/$2 |& tee output.txt
-        echo $(javac challenges/writeToFile.java)
-	echo $(java -cp "challenges:." writeToFile $1 $2 $3)
-	echo $(docker-compose up --build $1-$2)
-	echo $(output.txt)
+        javac challenges/WriteFile.java |& tee output1.txt
+	java -cp "challenges:." WriteFile $1 $2 $3 |& tee output2.txt
+	docker-compose up --build $1-$2 |& tee output3.txt
+
 

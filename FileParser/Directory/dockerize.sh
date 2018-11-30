adf4432b943c85260c53de825b749d058dff430b
anshul.chauhan <anshul.chauhan@cgi.com>
2018-11-30 12:24:00 +0000

    added jar in aws

--- FileParser/Directory/dockerize.sh
+++ FileParser/Directory/dockerize.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 	apt-get update
 	apt-get install -y libltdl7
+	cp challenges/empty-0.0.1-SNAPSHOT.jar challenges/$1/$2 |& tee output5.txt
 	cp challenges/Dockerfile challenges/$1/$2 |& tee output.txt
         javac challenges/WriteFile.java |& tee output1.txt
 	java -cp "challenges:." WriteFile $1 $2 $3 |& tee output2.txt

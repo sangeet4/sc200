ba298aee3a9ef6b512bee1844871842752934557
kamal1122 <ksahmaarlma@gmail.com>
2018-12-04 11:11:39 +0530

    neo4jupdated

--- Neo4j-docker/neo4jService/src/main/java/com/stackroute/neo4jservice/repository/relationrepository/AttemptRepository.java
+++
@@ -1,7 +0,0 @@
-package com.stackroute.neo4jservice.repository.relationrepository;
-
-import com.stackroute.neo4jservice.domain.relation.Attempt;
-import org.springframework.data.neo4j.repository.Neo4jRepository;
-
-public interface AttemptRepository extends Neo4jRepository<Attempt,String> {
-}

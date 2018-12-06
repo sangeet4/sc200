b05e1585febcd645440403d5b8ece3af15592e2b
anshul <its-Anshul>
2018-12-06 16:12:31 +0530

    file controller changed

--- FileParser/src/main/java/com/sc200/controller/FileController.java
+++ FileParser/src/main/java/com/sc200/controller/FileController.java
@@ -36,7 +36,10 @@ public class FileController {
 		            {
 				System.out.println(" hello " + request.getTextFile().get(i));
 				System.out.println("content" + request.getFileContent().get(i));
-				Files file = new Files("challenges/" + request.getChallengeId() + "/" + request.getUserId() + "/" + request.getTextFile().get(i) , request.getFileContent().get(i) , "java");
+				int firstIndex = request.getTextFile().get(i).indexOf("/");
+                String directory1 = request.getTextFile().get(i).substring(0, firstIndex);
+                        System.out.println(Directory);
+                Files file = new Files("challenges/" + request.getChallengeId() + "/" + request.getUserId() + "/" + request.getTextFile().get(i) , request.getFileContent().get(i) , "java");
                                 String a = fileService.parseFile(file);
 		            }
 		            return "Succesfully Created";

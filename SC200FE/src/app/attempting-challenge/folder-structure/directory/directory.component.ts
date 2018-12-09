1229b3d68581e81400d421a2d28bcc00a50731ba
anshul.chauhan <anshul.chauhan@cgi.com>
2018-12-09 23:59:46 +0530

    Update directory.component.ts

--- SC200FE/src/app/attempting-challenge/folder-structure/directory/directory.component.ts
+++ SC200FE/src/app/attempting-challenge/folder-structure/directory/directory.component.ts
@@ -165,6 +165,7 @@ export class DirectoryComponent {
     console.log(this.challengeId + ' ' + this.userName);
     this.filesService.getTemplate("Template/" + this.challengeId).subscribe(data => {
       this.filesService.allFiles = data['paths'];
+      this.filesService.allFiles = data['contents'];
       console.log(data['paths']);
       this.database.initialize();
     });
